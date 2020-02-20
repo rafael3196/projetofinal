4e9ffd4b35332579c081c940c756ac8184550891
Rafael <rafael.3196@hotmail.com>
2020-02-19 21:34:27 -0300

    update 19/02

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-	include('conexao.php');
 	session_start();
-	if(isset($_POST['btlogin'])){ //Verificação de login e senha.
+	if(isset($_POST['btlogin'])){
 		$login = $_POST['tlogin'];
 		$senha = $_POST['tsenha'];
 		$sql = "select * from funcionario where f_status=1 and login='$login' and senha='$senha'";
@@ -24,52 +23,25 @@
 ?>
 
 <!DOCTYPE >
-
-<head>
-	<meta charset="utf-8"/>
-	<title>Pizzaria Saborella</title>
-<link rel="stylesheet" type="text/css" href="css/estilo.css"/>
-<script type="text/javascript" src="_javascript/javascript.js"></script>
-<link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
-
-<style>
-.teste {
-     float: left;
-	 margin: 5px;
-	 padding: 15px;
-	 width: 300px;
-	 heigth: 300px;
-	 border: 1px solid black;
-} 
-body {
-background: url(imagens/10.jpg)no-repeat center fixed;
--webkit-background-size: cover;
--moz-background-size: cover;
--o-background-size: cover;
-background-size: cover;
-}  
-</style>
-
-</head>
-<body>
-  
-
-<div id="login" class="bradius">
-	<div class="message"></div>
-	<div class="logo"><img src=" imagens/logoT.png"/></div>
-	<div Class="acomodar">
-	  <form action=""method="post" > <!--onSubmit="validacaoLogin()"--> 
-	  <label for="usuario">Usuário:<input id="tlogin" type="txt" class="txt bradius" name="tlogin" value="" required/>
-	  <label for="senha">Senha:<input id="tsenha"type="password" class="txt bradius" name="tsenha" value="" required/><p>
-	  <a href="esqueceusenha.php">Esqueceu Senha ?</a>
-	   <input type="submit" class="sb bradius" name="btlogin" value="Entrar"/> <!--onclick="return validacaoLogin()"-->
-	  
-	  </form>
-	  <!--acomodar-->
-	  </div>
-	  <!--login-->
-	 </div>
-	
-
-	  </body>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Pizzaria Saborella</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	</head>
+	<body>
+		<div id="login-box">
+			<div id="login-logo-image">
+				<img src=" imagens/logoT.png"/>
+			</div>
+			<div class="login-form">
+				<form action=""method="post" >
+					Usuário:<input type="text" class="login-form login-form-text" name="tlogin" required/>
+					Senha:<input type="password" class="login-form login-form-text" name="tsenha" required/>
+					<a href="esqueceusenha.php">Esqueceu Senha?</a>
+					<input type="submit" id="login-bt" class="login-form" name="btlogin" value="Entrar"/>
+				</form>
+			</div>
+		</div>
+	</body>
 </html>
